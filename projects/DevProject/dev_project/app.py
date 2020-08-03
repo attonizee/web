e67cd787c7020576aa9572bf197993366c91889f
Anatolii Veselyi <anatoliiv1990@gmail.com>
2020-08-03 23:41:08 +0300

    flask web app v2

--- projects/DevProject/dev_project/app.py
+++ projects/DevProject/dev_project/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from data import archers
+from data import l2_classes
 
 
 app = Flask(__name__)
@@ -10,4 +10,8 @@ app.config['SECRET_KEY'] = 'xoo2Eim1'
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html', archers=archers)
+    return render_template('index.html', l2_classes=l2_classes)
+
+@app.route('/classes/<l2_class>')
+def classes(l2_class):
+    return render_template('classes.html', template_class_name = l2_class, template_l2_class = l2_classes[l2_class])
--- projects/DevProject/dev_project/data.py
+++ projects/DevProject/dev_project/data.py
@@ -1,13 +1,14 @@
-archers = {'Elven Archer': {'Race': 'Elf', 'Armor': 'Light Armor', 'Weapon': 'Bow', 'Role': 'Deal Damage', 'id': 'elven_archer', 'description': '''The Silver Ranger is the ideal Elven fighter in terms of skill with the bow.<br>
-                    A Silver Ranger's key skills are Double Shot, which shoots two arrows consecutively, <br>
+l2_classes = {'Archers': {'Elven Archer': {'Race': 'Elf', 'Armor': 'Light Armor', 'Weapon': 'Bow', 'Role': 'Deal Damage', 'id': 'elven_archer', 'description': '''The Silver Ranger is the ideal Elven fighter in terms of skill with the bow.
+                    A Silver Ranger's key skills are Double Shot, which shoots two arrows consecutively, 
                     and Burst Shot, which hits multiple enemies within range.'''},
-'Human Archer': {'Race': 'Human', 'Armor': 'Light Armor', 'Weapon': 'Bow', 'Role': 'Deal Damage', 'id': 'human_archer', 'description': '''Rather than using daggers, the Hawkeye threatens opponents from afar <br>
-                     using a longbow. Hawkeyes become even more powerful as part of a party. <br>
-                     The Hawkeye's aim is accurate against single opponents, and a Hawkeye can also<br>
+'Human Archer': {'Race': 'Human', 'Armor': 'Light Armor', 'Weapon': 'Bow', 'Role': 'Deal Damage', 'id': 'human_archer', 'description': '''Rather than using daggers, the Hawkeye threatens opponents from afar
+                     using a longbow. Hawkeyes become even more powerful as part of a party. 
+                     The Hawkeye's aim is accurate against single opponents, and a Hawkeye can also
                       attack groups of enemies effectively with skills such as Burst Shot.'''},
-'Dark Elf Archer': {'Race': 'Dark Elf', 'Armor': 'Light Armor', 'Weapon': 'Bow', 'Role': 'Deal Damage', 'id': 'd_elf_archer', 'description':'''The Phantom Ranger handles a bow skillfully and uses techniques similar to those<br>
-                 of other Archers, but Phantom Rangers also have excellent attack strength.<br>
-                 By weakening the defensive strength of an opponent through Hex,<br>
-                  the Phantom Ranger can harass adversaries at long distances using Double Shot,<br>
+'Dark Elf Archer': {'Race': 'Dark Elf', 'Armor': 'Light Armor', 'Weapon': 'Bow', 'Role': 'Deal Damage', 'id': 'd_elf_archer', 'description':'''The Phantom Ranger handles a bow skillfully and uses techniques similar to those
+                 of other Archers, but Phantom Rangers also have excellent attack strength.
+                 By weakening the defensive strength of an opponent through Hex,
+                  the Phantom Ranger can harass adversaries at long distances using Double Shot,
                    which shoots two arrows consecutively.'''}
+}, 'Warriors': {}
 }
