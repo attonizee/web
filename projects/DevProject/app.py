98ff73047a2f4a2d78ffe758b548744eec195a50
Anatolii Veselyi <anatoliiv1990@gmail.com>
2020-08-25 23:00:51 +0300

    few CSS things

--- projects/DevProject/app.py
+++ projects/DevProject/app.py
@@ -4,8 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Classes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 import routes
+
+if __name__=='__main__':
+    app.run()
