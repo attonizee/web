6974306522b7c4dbc22a3f7bd8a0c341f1d0b904
Anatolii Veselyi <anatoliiv1990@gmail.com>
2020-08-31 21:34:54 +0300

    updates on registration

--- projects/food_for_office/routes.py
+++ projects/food_for_office/routes.py
@@ -3,6 +3,13 @@ from flask import render_template, request
 from model import Clients, Company, Order, Products
 from forms import RegistrationForm
 
+
+@app.route('/newuser')
+def new_user():
+    reg_form = RegistrationForm()
+    return render_template('registration.html', template_form = reg_form)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     reg_form = RegistrationForm()
@@ -10,5 +17,11 @@ def register():
         client = Clients(name = reg_form.username.data, email = reg_form.email.data)
         client.set_password(reg_form.password.data)
         db.session.add(client)
-        db.session.commit()
-    return render_template('registration.html', template_form = reg_form)
+        try:
+            db.session.commit()
+            return render_template('registered.html',template_user = client )
+        except Exception:
+            db.session.rollback()
+            print('Something wrong')
+            return '<h2>This user already exist</h2>'
+    
